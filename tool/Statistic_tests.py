3c9a17a4c954620c892a698560fa51f548960e3c
mel361 <melkerelof@gmail.com>
2025-04-25 11:50:09 +0200

    Added code to answer RQ3, also started making the test iterable by looping a lot of the same test to ensure quality

--- tool/Statistic_tests.py
+++ tool/Statistic_tests.py
@@ -1,63 +1,31 @@
 from scipy.stats import spearmanr, shapiro, pearsonr
 
+def correlation_test(list_y, list_x, label_y, label_x):
+    stat_x, p_x = shapiro(list_x)
+    stat_y, p_y = shapiro(list_y)
 
+    print("\n📊 Shapiro-Wilk Normality Test for " + label_x.capitalize() + " and " + label_y.capitalize())
+    print("Shapiro-Wilk for " + label_x + f" drift: stat={stat_x:.4f}, p={p_x:.4f}")
+    print("Shapiro-Wilk for " + label_y + f": stat={stat_y:.4f}, p={p_y:.4f}")
 
-def correlation_precision_test(precision_list, full_batch_drift_mean_list):
-    stat_drift, p_drift = shapiro(full_batch_drift_mean_list)
-    stat_precision, p_precision = shapiro(precision_list)
-
-    print(f"Shapiro-Wilk for full-batch Drift: stat={stat_drift:.4f}, p={p_drift:.4f}")
-    print(f"Shapiro-Wilk for Precision: stat={stat_precision:.4f}, p={p_precision:.4f}")
-
-    if p_drift < 0.05 or p_precision < 0.05:
-        corr, pval = spearmanr(full_batch_drift_mean_list, precision_list)
+    if p_x < 0.05 or p_y< 0.05:
+        corr, pval = spearmanr(list_x, list_y)
 
         print("\n📊 Spearman Rank Correlation Test")
         print(f"Correlation coefficient (ρ): {corr:.3f}")
         print(f"p-value: {pval:.4f}")
     else:
-        corr, pval = pearsonr(full_batch_drift_mean_list, precision_list)
+        corr, pval = pearsonr(list_x, list_y)
 
         print("\n📊 Pearson Correlation Test")
         print(f"Correlation coefficient (ρ): {corr:.3f}")
         print(f"p-value: {pval:.4f}")
 
     if pval < 0.05:
-        print("➡️ Significant correlation between drift and precision (p < 0.05)")
+        print("➡️ Significant correlation between " + label_x + " drift and " + label_y + " (p < 0.05)")
         if corr > 0:
-            print("📈 Positive correlation – higher drift tends to give higher precision")
+            print("📈 Positive correlation – higher drift tends to give higher " + label_y + "\n\n")
         else:
-            print("📉 Negative correlation – higher drift tends to give lower precision")
+            print("📉 Negative correlation – higher drift tends to give lower " + label_y + "\n\n")
     else:
-        print("ℹ️ No significant correlation (p ≥ 0.05)")
-
-
-
-def correlation_recall_test(recall_list, full_batch_drift_mean_list):
-    stat_drift, p_drift = shapiro(full_batch_drift_mean_list)
-    stat_precision, p_precision = shapiro(recall_list)
-
-    print(f"Shapiro-Wilk for full-batch Drift: stat={stat_drift:.4f}, p={p_drift:.4f}")
-    print(f"Shapiro-Wilk for Recall: stat={stat_precision:.4f}, p={p_precision:.4f}")
-
-    if p_drift < 0.05 or p_precision < 0.05:
-        corr, pval = spearmanr(full_batch_drift_mean_list, recall_list)
-
-        print("\n📊 Spearman Rank Correlation Test")
-        print(f"Correlation coefficient (ρ): {corr:.3f}")
-        print(f"p-value: {pval:.4f}")
-    else:
-        corr, pval = pearsonr(full_batch_drift_mean_list, recall_list)
-
-        print("\n📊 Pearson Correlation Test")
-        print(f"Correlation coefficient (ρ): {corr:.3f}")
-        print(f"p-value: {pval:.4f}")
-
-    if pval < 0.05:
-        print("➡️ Significant correlation between drift and recall (p < 0.05)")
-        if corr > 0:
-            print("📈 Positive correlation – higher drift tends to give higher recall")
-        else:
-            print("📉 Negative correlation – higher drift tends to give lower recall")
-    else:
-        print("ℹ️ No significant correlation (p ≥ 0.05)")
+        print("ℹ️ No significant correlation (p ≥ 0.05)\n\n")
--- tool/full-batch_monitoring_tool.py
+++ tool/full-batch_monitoring_tool.py
@@ -44,81 +44,113 @@ report = Report([
 ])
 
 
-# Collect metrics for the graphs
-chunk_ids = []
-full_batch_drift_mean_list = []
+# Initialize lists and dicts
+full_time_list = []
+all_iteration_times = {}
+full_batch_drift_values = {}
+full_batch_feature_drift_values = {feature: {} for feature in FRAUD_FEATURES}
 
-# To keep track of drift scores for each feature
-full_batch_feature_drifts = {}
+for i in range(10):
+    # Set the chunk size
+    chunk_size = CHUNK_SIZE
+    n = 1
+    full_timer_start = time.time()
+    time_drift_detected = 0
 
-# Set the chunk size
-chunk_size = CHUNK_SIZE
-n = 1
-full_timer_start = time.time()
-time_drift_detected = 0
-
-# Initialize a list to store iteration times
-iteration_times = []
-# Simulate real-time data drift monitoring
-for i in range(0, len(test_X), chunk_size):
-    iteration_time = time.time()
-    print("Processing chunk: ", n, "////////////////////////////////////////")
-    current_batch = test_X.iloc[0:i + chunk_size]
-
-    chunk_ids.append(n)
-
-    # Empty lists to store drift scores
-    full_batch_drift_scores = []
-
-    # Create an Evidently report
-    report.run(
-        current_data=current_batch,
-        reference_data=train_X,
-        column_mapping=column_mapping
-    )
-    result_full_batch = report.as_dict()
-
-    data_drift = False
+    # Initialize a list to store iteration times
+    iteration_times = []
+    per_chunk_drift = []
+    per_feature_chunk_drift = {feature: [] for feature in FRAUD_FEATURES}
+    # Simulate real-time data drift monitoring
+    for j in range(0, len(test_X), chunk_size):
+        iteration_time = time.time()
+        print("Processing chunk: ", n, "////////////////////////////////////////")
+        current_batch = test_X.iloc[0:j + chunk_size]
 
 
-    # Check for data drift in the batch
-    print("Checking for drift in current batch")
-    for metric in result_full_batch["metrics"]:
-        if metric["metric"] == "DataDriftTable":
-            drift_by_columns = metric["result"].get("drift_by_columns", {})
-            for feature_name, feature_data in drift_by_columns.items():
-                drift_score = feature_data["drift_score"]
-                full_batch_drift_scores.append(drift_score)
-                if drift_score > 0.1:
-                    time_drift_detected = time.time() - full_timer_start
-                    print(f"⚠️ Drift in '{feature_name}': {drift_score:.3f}")
-                    data_drift = True
+        # Empty lists to store drift scores
+        full_batch_drift_scores = []
 
-                if feature_name not in full_batch_feature_drifts:
-                    full_batch_feature_drifts[feature_name] = []
-                full_batch_feature_drifts[feature_name].append(drift_score)
+        # Create an Evidently report
+        report.run(
+            current_data=current_batch,
+            reference_data=train_X,
+            column_mapping=column_mapping
+        )
+        result_full_batch = report.as_dict()
 
-    # Calculate the mean drift scores for full_batch
-    full_batch_drift_mean = np.mean(full_batch_drift_scores) if full_batch_drift_scores else 0
-    full_batch_drift_mean_list.append(full_batch_drift_mean)
-
-    if not data_drift:
-        print("✅ No significant drift detected.")
+        chunk_drift_scores = []
+        data_drift = False
 
 
-    n += 1
-    iteration_times.append(time.time() - iteration_time)
+        # Check for data drift in the batch
+        print("Checking for drift in current batch")
+        for metric in result_full_batch["metrics"]:
+            if metric["metric"] == "DataDriftTable":
+                drift_by_columns = metric["result"].get("drift_by_columns", {})
+                for feature_name, feature_data in drift_by_columns.items():
+                    drift_score = feature_data["drift_score"]
+                    chunk_drift_scores.append(drift_score)
+                    per_feature_chunk_drift[feature_name].append(drift_score)
+                    if drift_score > 0.1:
+                        time_drift_detected = time.time() - full_timer_start
+                        print(f"⚠️ Drift in '{feature_name}': {drift_score:.3f}")
+                        data_drift = True
+
+
+        if not data_drift:
+            print("✅ No significant drift detected.")
+
+        print("n: ", n)
+        per_chunk_drift.append(np.mean(chunk_drift_scores)) if chunk_drift_scores else 0
+
+
+        n += 1
+        iteration_times.append(time.time() - iteration_time)
+
+    print("Time drift detected: ", time_drift_detected)
+
+    for chunk_id in range(n - 1):
+        if chunk_id not in all_iteration_times:
+            all_iteration_times[chunk_id] = []
+        all_iteration_times[chunk_id].append(iteration_times[chunk_id])
+
+    for chunk_id, drift_val in enumerate(per_chunk_drift):
+        if chunk_id not in full_batch_drift_values:
+            full_batch_drift_values[chunk_id] = []
+        full_batch_drift_values[chunk_id].append(drift_val)
+
+    for feature in FRAUD_FEATURES:
+        for chunk_id, drift_val in enumerate(per_feature_chunk_drift[feature]):
+            if chunk_id not in full_batch_feature_drift_values[feature]:
+                full_batch_feature_drift_values[feature][chunk_id] = []
+            full_batch_feature_drift_values[feature][chunk_id].append(drift_val)
+
+    full_time_list.append(time_drift_detected)
 
 
 
 
+# 1. Mean iterations time per chunk
+mean_iteration_times = [np.mean(all_iteration_times[chunk_id]) for chunk_id in sorted(all_iteration_times)]
 
+# 2. Mean drift per chunk
+mean_drift_per_chunk = [np.mean(full_batch_drift_values[chunk_id]) for chunk_id in sorted(full_batch_drift_values)]
 
-print("Time drift detected: ", time_drift_detected)
+# 3. Mean drift per feature per chunk
+mean_feature_drift_per_chunk = {
+    feature: [np.mean(full_batch_feature_drift_values[feature][chunk_id])
+              for chunk_id in sorted(full_batch_feature_drift_values[feature])]
+    for feature in FRAUD_FEATURES
+}
+
+# 4. Mean full drift-tid
+mean_full_time = np.mean(full_time_list)
+print("Mean time drift detected: ", time_drift_detected)
 
 # --------- Iteration times
 plt.figure(figsize=(10, 4))
-plt.plot(range(1, len(iteration_times) + 1), iteration_times, marker='o')
+plt.plot(range(1, len(mean_iteration_times) + 1), mean_iteration_times, marker='o')
 plt.title("Iteration times")
 plt.xlabel("Iterations")
 plt.ylabel("Time (seconds)")
@@ -126,12 +158,23 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
+# --------- Iteration times
+plt.figure(figsize=(10, 4))
+plt.plot(range(1, len(full_time_list) + 1), full_time_list, marker='o')
+plt.title("Full-time times")
+plt.xlabel("tests")
+plt.ylabel("Time (seconds)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 
 
+print("full_batch_drift_mean_list SIZE: ", len(mean_drift_per_chunk))
+print("precision_list SIZE: ", len(precision_list))
 
 plt.figure(figsize=(8, 5))
-plt.scatter(full_batch_drift_mean_list, precision_list, alpha=0.7, label="Precision vs full-batch Drift")
-plt.scatter(full_batch_drift_mean_list, recall_list, alpha=0.7, label="Recall vs full-batch Drift")
+plt.scatter(mean_drift_per_chunk, precision_list, alpha=0.7, label="Precision vs full-batch Drift")
+plt.scatter(mean_drift_per_chunk, recall_list, alpha=0.7, label="Recall vs full-batch Drift")
 plt.xlabel("Mean full-batch Drift")
 plt.ylabel("Score")
 plt.title("Precision/Recall vs full-batch Drift")
@@ -145,7 +188,7 @@ plt.show()
 
 # Print trend for precision per drift score
 plt.figure(figsize=(8, 5))
-sns.regplot(x=full_batch_drift_mean_list, y=precision_list, lowess=True)
+sns.regplot(x=mean_drift_per_chunk, y=precision_list, lowess=True)
 plt.xlabel("Mean Full-batch Drift")
 plt.ylabel("Precision")
 plt.title("Drift vs Precision with Trend line")
@@ -155,7 +198,7 @@ plt.show()
 
 # Print trend for recall per drift score
 plt.figure(figsize=(8, 5))
-sns.regplot(x=full_batch_drift_mean_list, y=recall_list, lowess=True)
+sns.regplot(x=mean_drift_per_chunk, y=recall_list, lowess=True)
 plt.xlabel("Mean Full-batch Drift")
 plt.ylabel("Recall")
 plt.title("Drift vs Recall with Trend line")
@@ -165,6 +208,38 @@ plt.show()
 
 #Create_Graphs.print_graphs(chunk_ids, precision_list, recall_list, chunk_drift_mean_list, batch_drift_mean_list, chunk_feature_drifts, batch_feature_drifts)
 
-Statistic_tests.correlation_precision_test(precision_list, full_batch_drift_mean_list)
-Statistic_tests.correlation_recall_test(recall_list, full_batch_drift_mean_list)
+Statistic_tests.correlation_test(precision_list, mean_drift_per_chunk, "precision", "full-batch mean")
+Statistic_tests.correlation_test(recall_list, mean_drift_per_chunk, "recall", "full-batch mean")
+
+
+
+
+# Create graphs for each feature to visualize precision and recall per drift scores
+for feature in FRAUD_FEATURES:
+    # Print trend for precision per drift score
+    plt.figure(figsize=(8, 5))
+    sns.regplot(x=mean_feature_drift_per_chunk[feature], y=precision_list, lowess=True)
+    plt.xlabel(feature.capitalize() + " Full-batch Drift")
+    plt.ylabel("Precision")
+    plt.title(feature.capitalize() + ": Drift vs Precision with Trend line")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    Statistic_tests.correlation_test(precision_list, mean_feature_drift_per_chunk[feature], "precision", feature)
+
+    # Print trend for recall per drift score
+    plt.figure(figsize=(8, 5))
+    sns.regplot(x=mean_feature_drift_per_chunk[feature], y=recall_list, lowess=True)
+    plt.xlabel(feature.capitalize() + " Full-batch Drift")
+    plt.ylabel("Recall")
+    plt.title(feature.capitalize() + ": Drift vs Recall with Trend line")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    Statistic_tests.correlation_test(recall_list, mean_feature_drift_per_chunk[feature], "recall", feature)
+
+
+
 
