8bfd2a6912f7734fbe18624b9a7756dd9b43e892
mel361 <melkerelof@gmail.com>
2025-04-28 10:39:59 +0200

    Removed unused class, and created a class for constants

---
+++ tool/CONSTANT_VALUES.py
@@ -0,0 +1,26 @@
+# Constants for the monitoring tool
+
+# Threshold for detecting data drift. If the drift score exceeds this value, drift is flagged.
+DRIFT_THRESHOLD = 0.1
+
+# Number of rows to process in each chunk during batch monitoring.
+CHUNK_SIZE = 4000
+
+# List of features used for fraud detection. Drift will be monitored for these features.
+FRAUD_FEATURES = [
+    'income',
+    'name_email_similarity',
+    'prev_address_months_count',
+    'current_address_months_count',
+    'customer_age',
+    'days_since_request',
+    'intended_balcon_amount',
+    'zip_count_4w'
+]
+
+# File paths for input and output data used in the monitoring tool.
+PATH_TRAIN_X = "/model_values/train_X.csv"  # Path to the training dataset (features only).
+PATH_TEST_Y = "/model_values/test_y.csv"   # Path to the test dataset (labels only).
+PATH_TEST_X = "/model_values/test_X.csv"   # Path to the test dataset (features only).
+PATH_METRICS = "/model_values/metrics.csv" # Path to save evaluation metrics.
+PATH_BEST_THRESHOLD = "/model_values/best_threshold.json" # Path to save the best threshold values.
