81c7fca97ea88e7d77b38531475d9c6c2be22246
mel361 <melkerelof@gmail.com>
2025-05-08 11:32:52 +0200

    Changed to operativesystem independent paths

--- tool/CONSTANT_VALUES.py
+++ tool/CONSTANT_VALUES.py
@@ -1,5 +1,5 @@
 import os
-
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Constants for the monitoring tool
 
@@ -26,41 +26,46 @@ FRAUD_FEATURES = [
     'zip_count_4w'
 ]
 
-# File paths for input and output data used in the monitoring tool.
-PATH_TRAIN_X = "model_values\\train_X.csv"  # Path to the training dataset (features only).
-PATH_TEST_Y = "model_values\\test_y.csv"   # Path to the test dataset (labels only).
-PATH_TEST_X = "model_values\\test_X.csv"   # Path to the test dataset (features only).
-PATH_METRICS = "model_values\\metrics.csv" # Path to save evaluation metrics.
-PATH_BEST_THRESHOLD = "model_values\\best_threshold.json" # Path to save the best threshold values.
-PATH_FULL_BATCH_TOOL_STATISTICS_MEAN_DRIFTS = f"..\\{VARIANT_NAME}\\statistics\\full_batch_tool_statistics\\mean_drifts.csv"
-PATH_MICRO_BATCH_TOOL_STATISTICS_MEAN_DRIFTS = f"..\\{VARIANT_NAME}\\statistics\\micro_batch_tool_statistics\\mean_drifts.csv"
-PATH_FULL_BATCH_TOOL_STATISTICS_FEATURES = f"..\\{VARIANT_NAME}\\statistics\\full_batch_tool_statistics\\features.csv"
-PATH_MICRO_BATCH_TOOL_STATISTICS_FEATURES = f"..\\{VARIANT_NAME}\\statistics\\micro_batch_tool_statistics\\features.csv"
-PATH_FULL_BATCH_TOOL_STATISTICS_DETECTION_TIMES = f"..\\{VARIANT_NAME}\\statistics\\full_batch_tool_statistics\\detection_times.csv"
-PATH_MICRO_BATCH_TOOL_STATISTICS_DETECTION_TIMES = f"..\\{VARIANT_NAME}\\statistics\\micro_batch_tool_statistics\\detection_times.csv"
-PATH_FULL_BATCH_TOOL_STATISTICS_ITERATION_TIMES = f"..\\{VARIANT_NAME}\\statistics\\full_batch_tool_statistics\\iteration_times.csv"
-PATH_MICRO_BATCH_TOOL_STATISTICS_ITERATION_TIMES = f"..\\{VARIANT_NAME}\\statistics\\micro_batch_tool_statistics\\iteration_times.csv"
-PATH_FULL_BATCH_TOOL_STATISTICS_DETECTION_IDS = f"..\\{VARIANT_NAME}\\statistics\\full_batch_tool_statistics\\detection_ids.csv"
-PATH_MICRO_BATCH_TOOL_STATISTICS_DETECTION_IDS = f"..\\{VARIANT_NAME}\\statistics\\micro_batch_tool_statistics\\detection_ids.csv"
 
+PATH_REFERENCE_DATASET =  os.path.join(script_dir, '..', 'data', 'Reference.csv')# Path to the reference dataset used for training the model.
+PATH_NEW_DATASET =  os.path.join(script_dir, '..', 'data', 'NewData.csv') # Path to the new dataset used for testing the model.
+
+# File paths for input and output data used in the monitoring tool.
+PATH_TRAIN_X = os.path.join(script_dir, 'model_values', 'train_X.csv')
+PATH_TEST_Y = os.path.join(script_dir, 'model_values', 'test_y.csv')
+PATH_TEST_X = os.path.join(script_dir, 'model_values', 'test_X.csv')
+PATH_METRICS = os.path.join(script_dir, 'model_values', 'metrics.csv')
+PATH_BEST_THRESHOLD = os.path.join(script_dir, 'model_values', 'best_threshold.json')
+
+PATH_FULL_BATCH_TOOL_STATISTICS_MEAN_DRIFTS = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'full_batch_tool_statistics', 'mean_drifts.csv')
+PATH_MICRO_BATCH_TOOL_STATISTICS_MEAN_DRIFTS = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'micro_batch_tool_statistics', 'mean_drifts.csv')
+PATH_FULL_BATCH_TOOL_STATISTICS_FEATURES = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'full_batch_tool_statistics', 'features.csv')
+PATH_MICRO_BATCH_TOOL_STATISTICS_FEATURES = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'micro_batch_tool_statistics', 'features.csv')
+PATH_FULL_BATCH_TOOL_STATISTICS_DETECTION_TIMES = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'full_batch_tool_statistics', 'detection_times.csv')
+PATH_MICRO_BATCH_TOOL_STATISTICS_DETECTION_TIMES = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'micro_batch_tool_statistics', 'detection_times.csv')
+PATH_FULL_BATCH_TOOL_STATISTICS_ITERATION_TIMES = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'full_batch_tool_statistics', 'iteration_times.csv')
+PATH_MICRO_BATCH_TOOL_STATISTICS_ITERATION_TIMES = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'micro_batch_tool_statistics', 'iteration_times.csv')
+PATH_FULL_BATCH_TOOL_STATISTICS_DETECTION_IDS = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'full_batch_tool_statistics', 'detection_ids.csv')
+PATH_MICRO_BATCH_TOOL_STATISTICS_DETECTION_IDS = os.path.join(script_dir, '..', VARIANT_NAME, 'statistics', 'micro_batch_tool_statistics', 'detection_ids.csv')
 
 # Paths for micro output graphs to be stored.
-PATH_MICRO = f"..\\{VARIANT_NAME}\\output_graphs\\micro-batch\\"
-PATH_GRAPH_MICRO_ITERATIONS = PATH_MICRO + "iteration_times.png"
-PATH_GRAPH_MICRO_FULL_TIME = PATH_MICRO + "full_time.png"
-PATH_GRAPH_MICRO_PRECISION_TREND = PATH_MICRO + "drift_vs_precision.png"
-PATH_GRAPH_MICRO_RECALL_TREND = PATH_MICRO + "drift_vs_recall.png"
-PATH_GRAPH_MICRO_FEATURES_PRECISION = "_drift_vs_precision.png"
-PATH_GRAPH_MICRO_FEATURES_RECALL = "_drift_vs_recall.png"
+PATH_MICRO = os.path.join(script_dir, '..', VARIANT_NAME, 'output_graphs', 'micro-batch')
+PATH_GRAPH_MICRO_ITERATIONS = os.path.join(PATH_MICRO, 'iteration_times.png')
+PATH_GRAPH_MICRO_FULL_TIME = os.path.join(PATH_MICRO, 'full_time.png')
+PATH_GRAPH_MICRO_PRECISION_TREND = os.path.join(PATH_MICRO, 'drift_vs_precision.png')
+PATH_GRAPH_MICRO_RECALL_TREND = os.path.join(PATH_MICRO, 'drift_vs_recall.png')
+PATH_GRAPH_MICRO_FEATURES_PRECISION = os.path.join(PATH_MICRO, '_drift_vs_precision.png')
+PATH_GRAPH_MICRO_FEATURES_RECALL = os.path.join(PATH_MICRO, '_drift_vs_recall.png')
+
 
 # Paths for full output graphs to be stored.
-PATH_FULL = f"..\\{VARIANT_NAME}\\output_graphs\\full-batch\\"
-PATH_GRAPH_FULL_ITERATIONS = PATH_FULL + "iteration_times.png"
-PATH_GRAPH_FULL_FULL_TIME = PATH_FULL + "full_time.png"
-PATH_GRAPH_FULL_PRECISION_TREND = PATH_FULL + "drift_vs_precision.png"
-PATH_GRAPH_FULL_RECALL_TREND = PATH_FULL + "drift_vs_recall.png"
-PATH_GRAPH_FULL_FEATURES_PRECISION = "_drift_vs_precision.png"
-PATH_GRAPH_FULL_FEATURES_RECALL = "_drift_vs_recall.png"
+PATH_FULL = os.path.join(script_dir, '..', VARIANT_NAME, 'output_graphs', 'full-batch')
+PATH_GRAPH_FULL_ITERATIONS = os.path.join(PATH_FULL, 'iteration_times.png')
+PATH_GRAPH_FULL_FULL_TIME = os.path.join(PATH_FULL, 'full_time.png')
+PATH_GRAPH_FULL_PRECISION_TREND = os.path.join(PATH_FULL, 'drift_vs_precision.png')
+PATH_GRAPH_FULL_RECALL_TREND = os.path.join(PATH_FULL, 'drift_vs_recall.png')
+PATH_GRAPH_FULL_FEATURES_PRECISION = os.path.join(PATH_FULL, '_drift_vs_precision.png')
+PATH_GRAPH_FULL_FEATURES_RECALL = os.path.join(PATH_FULL, '_drift_vs_recall.png')
 
 
 
