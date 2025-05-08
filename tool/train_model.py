0664b5fc5488ed5b4d94d656ef13026c5db5edac
mel361 <melkerelof@gmail.com>
2025-05-08 14:13:39 +0200

    Try new sizes

--- tool/train_model.py
+++ tool/train_model.py
@@ -85,8 +85,8 @@ best_threshold = best_f1Score[0]
 precision_list = []
 recall_list = []
 
-test_simulation_data_X = pd.concat([test_X[TEST_SIZE:], new_fraud_data[FRAUD_FEATURES].head(400000)], ignore_index=True)
-test_simulation_data_y = pd.concat([test_y[TEST_SIZE:], new_fraud_data['fraud_bool'].head(400000)], ignore_index=True)
+test_simulation_data_X = pd.concat([test_X[TEST_SIZE:], new_fraud_data[FRAUD_FEATURES].head(300000)], ignore_index=True)
+test_simulation_data_y = pd.concat([test_y[TEST_SIZE:], new_fraud_data['fraud_bool'].head(300000)], ignore_index=True)
 
 for i in range(0, len(test_simulation_data_X), CHUNK_SIZE):
     print("Processing chunk: ", i // CHUNK_SIZE, "////////////////////////////////////////")
