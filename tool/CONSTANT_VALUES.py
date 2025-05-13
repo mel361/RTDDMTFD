95d1e75c5b2d8c98ea743f0f03e29f5c66a4abde
mel361 <melkerelof@gmail.com>
2025-05-13 10:43:31 +0200

    Potential better f1-score

--- tool/CONSTANT_VALUES.py
+++ tool/CONSTANT_VALUES.py
@@ -17,25 +17,16 @@ TEST_ITERATIONS = 1
 # List of features used for fraud detection. Drift will be monitored for these features.
 FRAUD_FEATURES = [
     "prev_address_months_count",
-    "customer_age",
+    "current_address_months_count",
     "intended_balcon_amount",
     "zip_count_4w",
     "velocity_6h",
-    "velocity_24h",
     "velocity_4w",
-    "bank_branch_count_8w",
-    "date_of_birth_distinct_emails_4w",
-    "email_is_free",
-    "phone_home_valid",
-    "phone_mobile_valid",
     "bank_months_count",
-    "has_other_cards",
-    "proposed_credit_limit",
-    "foreign_request",
-    "session_length_in_minutes",
-    "keep_alive_session"
+    "keep_alive_session",
+    "phone_home_valid",
+    "device_distinct_emails_8w"
 ]
-
 
 
 PATH_REFERENCE_DATASET =  os.path.join(script_dir, '..', 'data', 'Reference.csv')# Path to the reference dataset used for training the model.
